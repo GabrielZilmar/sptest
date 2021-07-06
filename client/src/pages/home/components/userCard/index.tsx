9763734c4603756e56878af31e48bf9cb73b6535
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2021-07-06 16:21:17 -0300

    feat: user details card

--- client/src/pages/home/components/userCard/index.tsx
+++ client/src/pages/home/components/userCard/index.tsx
@@ -7,7 +7,10 @@ import {
   Avatar,
   Divider,
   Tooltip,
+  Modal,
 } from 'antd';
+
+import UserDetails from '../userDetails';
 
 interface IUserCardProps {
   id: number;
@@ -21,53 +24,77 @@ interface IUserCardProps {
 class UserCard extends React.Component<IUserCardProps> {
   state = {
     loading: false,
+    modalIsOpen: false,
+  }
+
+  handleCloseModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   render() {
     const {
       id, userName, avatarUrl, url, isAdmin,
     } = this.props;
-    const { loading } = this.state;
+    const { loading, modalIsOpen } = this.state;
 
     return (
-      <Card
-        loading={loading}
-        style={{
-          margin: 24,
-          borderRadius: 8,
-        }}
-      >
-        <Card.Meta
-          avatar={<Avatar src={avatarUrl || ''} />}
-          title={userName || ''}
-        />
-        <Divider />
-        <Row gutter={[12, 24]} justify="start">
-          <Col span={12} style={{ textAlign: 'left' }}>
-            <span style={{ fontWeight: 'bold' }}>ID: </span>
-            <span>{id || ''}</span>
-          </Col>
-          <Col span={12} style={{ textAlign: 'left' }}>
-            <span style={{ fontWeight: 'bold' }}>Perfil Url: </span>
-            <span>
-              <a
-                style={{ overflowWrap: 'break-word' }}
-                href={url || '/'}
-              >
-                <Tooltip title={url}>
-                  {`${url.substring(29)}`}
-                </Tooltip>
-              </a>
-            </span>
-          </Col>
-          <Col span={12} style={{ textAlign: 'left' }}>
-            <span style={{ fontWeight: 'bold' }}>Is Admin: </span>
-            {isAdmin
-              ? <span style={{ color: 'green' }}>YES</span>
-              : <span style={{ color: 'red' }}>NO</span>}
-          </Col>
-        </Row>
-      </Card>
+      <>
+        <Card
+          loading={loading}
+          style={{
+            margin: 24,
+            borderRadius: 8,
+          }}
+          hoverable
+          onClick={() => {
+            this.setState({ modalIsOpen: true });
+          }}
+        >
+          <Card.Meta
+            avatar={<Avatar src={avatarUrl || ''} />}
+            title={userName || ''}
+          />
+          <Divider />
+          <Row gutter={[12, 24]} justify="start">
+            <Col span={12} style={{ textAlign: 'left' }}>
+              <span style={{ fontWeight: 'bold' }}>ID: </span>
+              <span>{id || ''}</span>
+            </Col>
+            <Col span={12} style={{ textAlign: 'left' }}>
+              <span style={{ fontWeight: 'bold' }}>Perfil Url: </span>
+              <span>
+                <a
+                  style={{ overflowWrap: 'break-word' }}
+                  href={url || '/'}
+                >
+                  <Tooltip title={url}>
+                    {`${url.substring(29)}`}
+                  </Tooltip>
+                </a>
+              </span>
+            </Col>
+            <Col span={12} style={{ textAlign: 'left' }}>
+              <span style={{ fontWeight: 'bold' }}>Is Admin: </span>
+              {isAdmin
+                ? <span style={{ color: 'green' }}>YES</span>
+                : <span style={{ color: 'red' }}>NO</span>}
+            </Col>
+          </Row>
+        </Card>
+        <Modal
+          width="80vw"
+          visible={modalIsOpen}
+          onOk={this.handleCloseModal.bind(this)}
+          closable={false}
+          cancelButtonProps={{ style: { display: 'none' } }}
+        >
+          <UserDetails
+            login={userName}
+            avatar={avatarUrl}
+            loading={loading}
+          />
+        </Modal>
+      </>
     );
   }
 }
--- client/src/service/api/apiGit.ts
+++ client/src/service/api/apiGit.ts
@@ -5,6 +5,10 @@ const HttpClient = {
     method: 'get',
     url: '/users',
   }),
+  getUser: async (login: string) => fetchAPI({
+    method: 'get',
+    url: `/users/${login}/details`,
+  }),
 };
 
 export default HttpClient;
